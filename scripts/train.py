7c0c5d6a7a8970f90b6a7affb433cec56f5c5281
Daffan <zifanxu96@gmail.com>
2020-07-31 00:17:40 -0500

    add global goal len as rewardd

--- scripts/train.py
+++ scripts/train.py
@@ -3,9 +3,11 @@ import gym
 from jackal_sim_wrapper import *
 
 import numpy
+sys.path.remove('/opt/ros/melodic/lib/python2.7/dist-packages')
 from stable_baselines import DQN, PPO2, A2C, ACKTR
 from stable_baselines.common.cmd_util import make_vec_env
 from stable_baselines.common.callbacks import BaseCallback
+sys.path.append('/opt/ros/melodic/lib/python2.7/dist-packages')
 
 import pickle
 import argparse
